bb3c13d5d6ff99ad11bf085c0f1a0c9a9fd9c936
Alex Nacimento <alexdavi150@gmail.com>
2024-01-31 13:36:52 -0300

    feat: create formulario component

--- src/pages/formulario.tsx
+++ src/pages/formulario.tsx
@@ -7,23 +7,54 @@
  * - Os dois campos são obrigatórios e precisam de validação
  * - Ao dar 'submit', deve ser feito uma request para /api/users/create
  * - Lide com os possíveis erros
- */
+*/
 
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+import { TUserCreate } from '@/types/user';
 import styles from '@/styles/formulario.module.css';
 
-export default function Form() {
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
+type TErrorMessageProps = {
+	message: string | undefined;
+}
 
-		console.log('submit');
+function ErrorMessage({ message }: TErrorMessageProps) {
+	return (
+		<p>{message}</p>
+	)
+}
+
+export default function Form() {
+	const { register, handleSubmit, formState: {errors} } = useForm<TUserCreate>();
+
+	const onSubmit: SubmitHandler<TUserCreate> = data => {
+		fetch('/api/users/create', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
 	}
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
-				<form onSubmit={handleSubmit}>
-					<input type="text" placeholder="Name" />
-					<input type="email" placeholder="E-mail" />
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input {...register("name")} type="text" placeholder="Name" required />
+					<input
+						{...register("email", {
+							required: "Um email é necessário",
+							pattern: {
+								value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+								message: "Por favor, insira um email válido"
+							}
+						})}
+						type="text"
+						placeholder="E-mail"
+					/>
+
+					{errors?.email && <ErrorMessage message={errors?.email.message} />}
 
 					<button type="submit" data-type="confirm">
 						Enviar
