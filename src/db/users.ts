e7e9638a6cdedfe7fc2da76da7da94d5f6347a37
Alex Nacimento <alexdavi150@gmail.com>
2024-01-31 12:50:11 -0300

    fix: create getter and setter functions

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,4 +1,6 @@
-const usersList = [
+import { IUser, TUserCreate } from "@/types/user";
+
+let usersList: IUser[] = [
   {
     id: 1,
     name: 'John Doe',
@@ -14,6 +16,22 @@ const usersList = [
     name: 'Somebody Else',
     email: 'somebody@example.com'
   }
-]
+];
 
-export default usersList
+export function getUsers(): IUser[] {
+  return usersList;
+}
+
+export function createUser(user: TUserCreate): IUser {
+  const lastId: number = usersList[usersList.length - 1].id;
+  
+  const newUser: IUser = {
+    id: lastId + 1,
+    name: user.name,
+    email: user.email
+  }
+
+  usersList.push(newUser);
+
+  return newUser;
+}
