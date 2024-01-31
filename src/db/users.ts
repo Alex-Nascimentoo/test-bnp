5132ed679f794cd054998f67194aa7aa7238adc8
Alex Nacimento <alexdavi150@gmail.com>
2024-01-31 11:54:06 -0300

    feat: create a hard-coded db

---
+++ src/db/users.ts
@@ -0,0 +1,19 @@
+const usersList = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@example.com'
+  },
+  {
+    id: 2,
+    name: 'One Person',
+    email: 'oneperson@example.com'
+  },
+  {
+    id: 3,
+    name: 'Somebody Else',
+    email: 'somebody@example.com'
+  }
+]
+
+export default usersList
