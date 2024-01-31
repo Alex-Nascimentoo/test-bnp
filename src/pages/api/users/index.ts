2cff8f35f11f0aa0eab3897d0dbf1e5ad5142fbc
Alex Nacimento <alexdavi150@gmail.com>
2024-01-31 11:54:21 -0300

    feat: make api return data

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -13,9 +13,14 @@
 import { NextApiRequest, NextApiResponse } from 'next/types';
 
 import { IUser } from '@/types/user.d';
+import usersList from '@/db/users';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	const users: Array<unknown> = [];
+	const users: Array<IUser> = usersList;
 
-	return res.status(500).json(users);
+	if (req.method !== 'GET') {
+		return res.status(405).json({ ok: false, message: 'Method not allowed', ptMessage: 'Método não permitido' });
+	}
+
+	return res.status(200).json(users)
 };
